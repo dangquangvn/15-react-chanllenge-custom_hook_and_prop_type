0d99566bcf09aa34e71c7b5e285acba739e83d92
dangquangvn <61213745+dangquangvn@users.noreply.github.com>
2021-12-20 21:55:58 +0700

    [REACT ROUTER] setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import "./styles.css";
 import Example from "./hooks/1-fetch-example";
 import Proptypes from "./propTypes";
+import ReactRouterSetup from "./reactRouter";
 
 export default function App() {
-  return (
-    <div className="App">
-      <Proptypes />
-    </div>
-  );
+  return <div className="App">{<ReactRouterSetup />}</div>;
 }

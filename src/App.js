9eea9e64af3f67898e8b1ee2180f4445abf4b4a6
dangquangvn <61213745+dangquangvn@users.noreply.github.com>
2021-12-20 16:58:12 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import Example from "./hooks/1-fetch-example";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Example />
+    </div>
+  );
+}

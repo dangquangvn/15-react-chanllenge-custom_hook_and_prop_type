83acfaa63e3ab2508b3a73d363209385ce992810
dangquangvn <61213745+dangquangvn@users.noreply.github.com>
2021-12-21 14:43:50 +0700

    setup React.memo

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import "./styles.css";
 import Example from "./hooks/1-fetch-example";
 import Proptypes from "./propTypes";
 import ReactRouterSetup from "./reactRouter";
+import MemoUseMemoUseCallback from "./memo-useMemo-useCallback";
 
 export default function App() {
-  return <div className="App">{<ReactRouterSetup />}</div>;
+  return <div className="App">{<MemoUseMemoUseCallback />}</div>;
 }
